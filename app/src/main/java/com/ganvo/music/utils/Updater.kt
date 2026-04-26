c6fab873f325cd3bbe344a8542e116216c85ffa8
cgens67 <cgenshin587@gmail.com>
2026-04-26 22:45:27 +0800

    Update Updater.kt

--- app/src/main/java/com/ganvo/music/utils/Updater.kt
+++ app/src/main/java/com/ganvo/music/utils/Updater.kt
@@ -13,7 +13,7 @@ object Updater {
     suspend fun getLatestVersionName(): Result<String> =
         runCatching {
             val response =
-                client.get("https://api.github.com/repos/Ganvo/Ganvo/releases/latest")
+                client.get("https://api.github.com/repos/cgens67/ganvomusic/releases/latest")
                     .bodyAsText()
             val json = JSONObject(response)
             val versionName = json.getString("name")
